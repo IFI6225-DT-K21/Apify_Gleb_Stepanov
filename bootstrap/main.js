86356e3ae53d5973966e0abf587542025fc6433e
Gleb <gstepa@ttu.ee>
2021-05-24 21:04:23 +0300

    collapse companies

---
+++ bootstrap/main.js
@@ -0,0 +1,12 @@
+function myFunction() {
+    var x = document.getElementById("collapsePictures");
+    var y = document.getElementById("buttonCol");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+      y.style.display = "none";
+    } else {
+      x.style.display = "none";
+      y.style.display = "none";
+    }
+  }
+  
